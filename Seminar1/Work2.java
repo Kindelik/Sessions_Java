b062b5ff366ff97b7817465aed44d5d1793933f7
Kindelik <kindelik@yandex.ru>
2023-03-02 23:00:56 +0300

    Sem1

--- Seminar1/Work2.java
+++ Seminar1/Work2.java
@@ -1,16 +1,15 @@
 package Seminar1;
 
 public class Work2 {
-    public static void main() {
+    public static void main(String[] args) {
         int[] massiv = { 1, 1, 0, 1, 1, 1 };
         int count = 0;
         int max = 0;
-        for (int i = 0; i < massiv.length; i++) {
-            if (massiv[i]>0){
-                count +=1;
-            }
-            else{
-                if (count>max){
+        for (int j : massiv) {
+            if (j > 0) {
+                count += 1;
+            } else {
+                if (count > max) {
                     max = count;
                 }
                 count = 0;
