e8cc4b7ab473f32aced0882d8860c6c81ee24318
Kindelik <kindelik@yandex.ru>
2023-03-03 22:19:45 +0300

    Sem1End

--- Seminar1/Work3.java
+++ Seminar1/Work3.java
@@ -1,17 +1,16 @@
 package Seminar1;
 
-import java.lang.reflect.Array;
 import java.util.Arrays;
 
 public class Work3 {
     public static void main(String[] args) {
-        int[] arr = {1, 2, 3, 3, 1, 1, 5};
+        int[] arr = {1, 2, 3, 3, 1, 1, 5, 1};
         int number = 1;
         int temp = 0;
         int count = 0;
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] == number) {
-                arr[i] = arr[arr.length - count-1];
+                arr[i] = arr[arr.length - count - 1];
                 arr[arr.length - count - 1] = number;
                 count++;
                 System.out.println(Arrays.toString(arr));
