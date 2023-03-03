381bb4e371086ba82efd8d7f9776a2c242b0b5b9
Kindelik <kindelik@yandex.ru>
2023-03-03 22:39:38 +0300

    Hw2

--- HomeWork1/Work2.java
+++ HomeWork1/Work2.java
@@ -1,7 +1,17 @@
 package HomeWork1;
-
+//Вывести все простые числа от 1 до 1000
 public class Work2 {
     public static void main(String[] args) {
-
+        for (int i = 1; i < 1000; i++) {
+            if (simpleNumber(i)){
+                System.out.print(i + " ");
+            }
+        }
+    }
+    public static boolean simpleNumber(int num){
+        for (int i = 2; i <= num/2; i++) {
+        if (num%i ==0) return false;
+        }
+        return true;
     }
 }
