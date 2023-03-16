a04e7af55eac06f16a5c2b12f270b25829a1bd49
Kindelik <kindelik@yandex.ru>
2023-03-16 20:00:44 +0300

    HomeWork4Work2End,Work3NotEnd

--- test.java
+++ test.java
@@ -3,14 +3,11 @@ import java.util.Map;
 
 public class test {
     public static void main(String[] args) {
-        Map<String, String> states = new HashMap<>();
-        String somiStr = "{\"name\":\"Ivanov\", \"country\":\"Russia\", \"city\":\"Moscow\", \"age\":\"null\"}";
-        StringBuilder outStr = new StringBuilder();
-        String newInputStr = somiStr.replaceAll("[\"{}]", "");
-        System.out.println(newInputStr);
-        String[] newInputStrArr = newInputStr.split(",");
-        for (String item: newInputStrArr
-             ) {
+
+
+        Object last = null;
+        if (last == null){
+            System.out.println("ok");
 
         }
     }
